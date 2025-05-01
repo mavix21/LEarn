d4bebfe17083badf4c3111f23a8677cd7ceb52f8
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-04-30 23:44:36 -0500

    fix: update motion component creation to use HTMLElementTagNameMap for improved type safety

--- packages/ui/src/components/animated-group.tsx
+++ packages/ui/src/components/animated-group.tsx
@@ -118,11 +118,11 @@ function AnimatedGroup({
   const itemVariants = variants?.item ?? selectedVariants.item;
 
   const MotionComponent = React.useMemo(
-    () => motion.create(as as keyof JSX.IntrinsicElements),
+    () => motion.create(as as keyof HTMLElementTagNameMap),
     [as],
   );
   const MotionChild = React.useMemo(
-    () => motion.create(asChild as keyof JSX.IntrinsicElements),
+    () => motion.create(asChild as keyof HTMLElementTagNameMap),
     [asChild],
   );
 
