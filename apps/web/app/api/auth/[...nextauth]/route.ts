2207187d13b59f13fcc480809dd2b529a57e5ba1
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-02 18:20:51 -0500

    refactor: update authentication implementation and middleware for NextAuth integration

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,105 @@
-import { handlers } from "@/auth";
+import type { SIWESession } from "@reown/appkit-siwe";
+import {
+  getAddressFromMessage,
+  getChainIdFromMessage,
+} from "@reown/appkit-siwe";
+import NextAuth from "next-auth";
+import credentialsProvider from "next-auth/providers/credentials";
+import { createPublicClient, http } from "viem";
 
-export const { GET, POST } = handlers;
+import { env } from "@/src/env";
+
+declare module "next-auth" {
+  interface Session extends SIWESession {
+    address: string;
+    chainId: number;
+  }
+}
+
+const authSecret = env.NEXTAUTH_SECRET;
+const projectId = env.NEXT_PUBLIC_WC_PROJECT_ID;
+
+const providers = [
+  credentialsProvider({
+    name: "Ethereum",
+    credentials: {
+      message: {
+        label: "Message",
+        type: "text",
+        placeholder: "0x0",
+      },
+      signature: {
+        label: "Signature",
+        type: "text",
+        placeholder: "0x0",
+      },
+    },
+    async authorize(credentials) {
+      console.log("credentials", credentials);
+      try {
+        if (!credentials?.message) {
+          throw new Error("Siwe message or signature is undefined");
+        }
+
+        const message = credentials.message;
+        const signature = credentials.signature;
+
+        const address = getAddressFromMessage(message);
+        const chainId = getChainIdFromMessage(message);
+
+        // const isValid = await verifySignature({
+        //   address,
+        //   message,
+        //   signature,
+        //   chainId,
+        //   projectId,
+        // });
+
+        const publicClient = createPublicClient({
+          transport: http(
+            `https://rpc.walletconnect.org/v1/?chainId=${chainId}&projectId=${projectId}`,
+          ),
+        });
+        const isValid = await publicClient.verifyMessage({
+          message,
+          address: address as `0x${string}`,
+          signature: signature as `0x${string}`,
+        });
+
+        if (!isValid) return null;
+
+        return {
+          id: `${chainId}:${address}`,
+        };
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    },
+  }),
+];
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const handler = NextAuth({
+  secret: authSecret,
+  providers,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    session({ session, token }) {
+      if (!token.sub) return session;
+
+      const [, chainId, address] = token.sub.split(":");
+
+      if (chainId && address) {
+        session.address = address;
+        session.chainId = parseInt(chainId, 10);
+      }
+
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,15 +1,62 @@
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
 import createMiddleware from "next-intl/middleware";
 
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const i18nMiddleware = createMiddleware(routing);
+
+const withAuthMiddleware = withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const isAuthPage =
+      req.nextUrl.pathname.startsWith("/login") ||
+      req.nextUrl.pathname.startsWith("/register");
+
+    console.log("middleware", { token, isAuth, isAuthPage });
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return i18nMiddleware(req);
+        // return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+
+      return null;
+    }
+
+    if (!isAuth) {
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return i18nMiddleware(req);
+      // return NextResponse.redirect(
+      //   new URL(`/login?from=${encodeURIComponent(from)}`, req.url),
+      // );
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true;
+      },
+    },
+  },
+);
+
+export default withAuthMiddleware;
 
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
   matcher: [
-    "/((?!api|trpc|_next|_vercel|.*\\..*).*)", // Match all pathnames within `{/:locale}/users`
+    "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
     "/([\\w-]+)?/profile/(.+)",
   ],
 };
