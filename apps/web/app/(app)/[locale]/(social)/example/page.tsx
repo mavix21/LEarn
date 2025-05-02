7934577d1df12b895b157cbb2b65bb89286783d3
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-02 12:52:29 -0500

    test: add example route to test connect button

---
+++ apps/web/app/(app)/[locale]/(social)/example/page.tsx
@@ -0,0 +1,9 @@
+import "@reown/appkit-wallet-button/react";
+
+export default function Home() {
+  return (
+    <div className="col-start-1 row-span-1 row-start-1 self-center justify-self-center lg:col-start-2 lg:row-start-2">
+      <appkit-button />
+    </div>
+  );
+}
