84859ec7e16a4efc9446729ee236f09257a7863e
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-03 00:02:35 -0500

    feat: implement Ethereum authentication with NextAuth and WalletConnect integration

---
+++ apps/web/auth.ts
@@ -0,0 +1,121 @@
+import type {
+  GetServerSidePropsContext,
+  NextApiRequest,
+  NextApiResponse,
+} from "next";
+import type { NextAuthOptions } from "next-auth";
+import {
+  getAddressFromMessage,
+  getChainIdFromMessage,
+} from "@reown/appkit-siwe";
+import { getServerSession } from "next-auth";
+import credentialsProvider from "next-auth/providers/credentials";
+import { createPublicClient, http } from "viem";
+
+import { env } from "@/src/env";
+
+import type { Id } from "./convex/_generated/dataModel";
+import { createUser } from "./src/users/create-user.action";
+
+const authSecret = env.NEXTAUTH_SECRET;
+const projectId = env.NEXT_PUBLIC_WC_PROJECT_ID;
+
+const providers = [
+  credentialsProvider({
+    name: "Ethereum",
+    credentials: {
+      message: {
+        label: "Message",
+        type: "text",
+        placeholder: "0x0",
+      },
+      signature: {
+        label: "Signature",
+        type: "text",
+        placeholder: "0x0",
+      },
+    },
+    async authorize(credentials) {
+      try {
+        if (!credentials?.message) {
+          throw new Error("Siwe message or signature is undefined");
+        }
+
+        const message = credentials.message;
+        const signature = credentials.signature;
+
+        const address = getAddressFromMessage(message);
+        const chainId = getChainIdFromMessage(message);
+
+        // const isValid = await verifySignature({
+        //   address,
+        //   message,
+        //   signature,
+        //   chainId,
+        //   projectId,
+        // });
+
+        const publicClient = createPublicClient({
+          transport: http(
+            `https://rpc.walletconnect.org/v1/?chainId=${chainId}&projectId=${projectId}`,
+          ),
+        });
+        const isValid = await publicClient.verifyMessage({
+          message,
+          address: address as `0x${string}`,
+          signature: signature as `0x${string}`,
+        });
+
+        if (!isValid) return null;
+
+        const userId = await createUser(address);
+
+        return {
+          id: `${chainId}:${address}:${userId}`,
+        };
+      } catch (error) {
+        console.error(error);
+        return null;
+      }
+    },
+  }),
+];
+
+export const config = {
+  secret: authSecret,
+  providers,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    session({ user, session, token }) {
+      console.log("****************** SESSION ******************", {
+        user,
+        session,
+        token,
+      });
+
+      if (!token.sub) return session;
+
+      const [, chainId, address, userId] = token.sub.split(":");
+
+      if (chainId && address && userId) {
+        session.address = address;
+        session.chainId = parseInt(chainId, 10);
+        session.userId = userId as Id<"users">;
+      }
+
+      return session;
+    },
+  },
+} satisfies NextAuthOptions;
+
+// Use it in server contexts
+export function auth(
+  ...args:
+    | [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+    | [NextApiRequest, NextApiResponse]
+    | []
+) {
+  return getServerSession(...args, config);
+}
