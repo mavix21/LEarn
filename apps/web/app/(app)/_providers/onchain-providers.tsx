d18a2406ff3603ac75eec256ebe4424d16f91446
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-03 00:05:20 -0500

    feat: update OnchainKit configuration to set default network and wallet preferences

--- apps/web/app/(app)/_providers/onchain-providers.tsx
+++ apps/web/app/(app)/_providers/onchain-providers.tsx
@@ -4,9 +4,9 @@ import type { ReactNode } from "react";
 import { useState } from "react";
 import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { createAppKit } from "@reown/appkit";
+import { base, baseSepolia } from "@reown/appkit/networks";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { cookieToInitialState, WagmiProvider } from "wagmi";
-import { base } from "wagmi/chains";
 
 import { env } from "@/src/env";
 
@@ -27,6 +27,8 @@ const projectId = env.NEXT_PUBLIC_WC_PROJECT_ID;
 
 // Create modal
 createAppKit({
+  defaultNetwork: baseSepolia,
+  coinbasePreference: "smartWalletOnly",
   adapters: [wagmiAdapter],
   networks: chains,
   projectId,
@@ -45,6 +47,11 @@ createAppKit({
     ],
     emailShowWallets: true, // default to true
   },
+  allWallets: "HIDE",
+  featuredWalletIds: [
+    "fd20dc426fb37566d803205b19bbc1d4096b248ac04548e3cfb6b3a38bd033aa", // coinbase
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", // metamask
+  ],
 });
 
 export function OnchainProviders({ children, cookie }: Props) {
