a3bf1b725444d6f5d1f7d6c532e046bd6ccff2a8
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-03 20:35:00 -0500

    feat: refactor routing to use constant for locales

--- apps/web/i18n/routing.ts
+++ apps/web/i18n/routing.ts
@@ -1,8 +1,10 @@
 import { defineRouting } from "next-intl/routing";
 
+export const locales = ["en", "es"] as const;
+
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "es"],
+  locales,
 
   // Used when no locale matches
   defaultLocale: "en",
