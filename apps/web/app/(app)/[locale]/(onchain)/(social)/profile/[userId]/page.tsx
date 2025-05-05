a8a93994507af2c6ea3749ed1758e8d7ed22a2cb
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-04 20:47:05 -0500

    feat: update profile page to accept userId as a parameter for dynamic rendering

--- apps/web/app/(app)/[locale]/(onchain)/(social)/profile/[userId]/page.tsx
+++ apps/web/app/(app)/[locale]/(onchain)/(social)/profile/[userId]/page.tsx
@@ -2,10 +2,16 @@ import React from "react";
 
 import { ProfilePage } from "@/app/_pages/profile";
 
-export default function Page() {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ userId: string }>;
+}) {
+  const { userId } = await params;
+
   return (
     <div className="col-start-2 row-start-2 h-full overflow-hidden">
-      <ProfilePage />
+      <ProfilePage userId={userId} />
     </div>
   );
 }
