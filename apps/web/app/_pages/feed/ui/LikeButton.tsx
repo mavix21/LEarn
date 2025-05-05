de95b409c963cc4cef07b9c555920ae6674e2369
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2025-05-04 19:56:22 -0500

    feat: add like or dislike functionality - post

--- apps/web/app/_pages/feed/ui/LikeButton.tsx
+++ apps/web/app/_pages/feed/ui/LikeButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { useMutation } from "convex/react";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "convex/react";
 import { Heart } from "lucide-react";
 
 import type { Id } from "@/convex/_generated/dataModel";
@@ -12,35 +12,50 @@ interface LikeButtonProps {
 }
 
 export function LikeButton({ postId }: LikeButtonProps) {
-  const [isLiked, setIsLiked] = useState(false);
   const createLike = useMutation(api.likes.createLikePost);
   const deleteLike = useMutation(api.likes.deleteLikePost);
+  const getLikes = useQuery(api.likes.getLikes, {
+    authorId: "j97f00n7t41er945tbhn0ddw057f466f" as Id<"users">,
+    postId: postId as Id<"posts">,
+  });
 
-  const handleLike = () => {
-    setIsLiked((prev) => !prev);
+  const [isLiked, setIsLiked] = useState<boolean | undefined>(undefined);
+
+  const handleLike = async () => {
+    if (isLiked === undefined) return;
     if (!isLiked) {
+      setIsLiked(true);
       createLike({
         postId: postId as Id<"posts">,
         authorId: "j97f00n7t41er945tbhn0ddw057f466f" as Id<"users">,
         authorAddress: "0x4029490B2Dedd37906F2911B444d081caAad8E71",
       }).catch((error) => {
+        setIsLiked(false);
         console.error("Failed to create like:", error);
       });
     } else {
+      setIsLiked(false);
       deleteLike({
         postId: postId as Id<"posts">,
         authorId: "j97f00n7t41er945tbhn0ddw057f466f" as Id<"users">,
       }).catch((error) => {
+        setIsLiked(true);
         console.error("Failed to delete like:", error);
       });
     }
   };
 
+  useEffect(() => {
+    if (getLikes !== undefined) {
+      setIsLiked(getLikes.length > 0);
+    }
+  }, [getLikes]);
+
   return (
     <button
-      // onClick={() => console.log("like or dislike")}
       onClick={handleLike}
       className="flex items-center transition-colors hover:text-red-500"
+      disabled={isLiked === undefined}
     >
       <Heart
         size={20}
