6257e32434eff1756bcfcc835ee60d4071219c95
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-07 22:12:31 -0500

    refactor: simplify ProfileCard component by removing unused imports and commented code

--- apps/web/app/_pages/feed/ui/ProfileCard.tsx
+++ apps/web/app/_pages/feed/ui/ProfileCard.tsx
@@ -8,18 +8,11 @@ import {
   Name,
   Socials,
 } from "@coinbase/onchainkit/identity";
-import {
-  ConnectWallet,
-  Wallet,
-  WalletDropdown,
-  WalletDropdownDisconnect,
-} from "@coinbase/onchainkit/wallet";
 import { Authenticated, Unauthenticated } from "convex/react";
 import { base } from "viem/chains";
 import { useAccount } from "wagmi";
 
-import { Button } from "@skill-based/ui/components/button";
-import { Card, CardAction, CardContent } from "@skill-based/ui/components/card";
+import { Card, CardContent } from "@skill-based/ui/components/card";
 import { Skeleton } from "@skill-based/ui/components/skeleton";
 import { cn } from "@skill-based/ui/lib/utils";
 
@@ -36,7 +29,7 @@ const Identity = dynamic(
 );
 
 export default function ProfileCard({ className }: { className?: string }) {
-  const { isConnected, address, isConnecting } = useAccount();
+  const { address } = useAccount();
   const { open } = useAppKit();
 
   console.log("address", address);
@@ -44,73 +37,31 @@ export default function ProfileCard({ className }: { className?: string }) {
   return (
     <div className={cn("w-full", className)}>
       <div className="flex flex-col items-center justify-center gap-2">
-        {/* <AvatarWithLoading
-          className="size-32"
-          address={address}
-          alt="Profile picture"
-          loadingComponent={<Skeleton className="size-32 rounded-full" />}
-        />
-
-        {isConnected && (
-          <NameWithLoading
-            className="h-6 text-xl font-bold"
-            address={address}
-            chain={base}
-          />
-        )} */}
-
-        {/* <IdentityCardWithLoading address={address} chain={base} />
-        <SocialsWithLoading address={address} chain={base} /> */}
         <Authenticated>
           {address && (
-            <Card
-              className="hover:!bg-muted group w-full cursor-pointer transition-colors"
-              onClick={() => open({ view: "Account" })}
-            >
-              <CardContent>
-                <Identity
+            <Card className="w-full">
+              <CardContent className="space-y-2">
+                <Avatar
                   address={address}
                   chain={base}
-                  className="group-hover:bg-muted transition-colors"
+                  className="mx-auto size-32 cursor-pointer"
+                  onClick={() => open({ view: "Account" })}
+                />
+                <Identity
+                  className="**:!ml-0 *:justify-center"
+                  address={address}
+                  chain={base}
                 >
-                  <Avatar className="block size-16" />
-                  <Name>
+                  <Name className="mx-auto">
                     <Badge tooltip="Coinbase verified account" />
                   </Name>
-                  <Address hasCopyAddressOnClick />
-                  <Socials />
+                  <Address className="text-center" hasCopyAddressOnClick />
+                  <Socials className="flex justify-center pl-0" />
                 </Identity>
               </CardContent>
             </Card>
           )}
         </Authenticated>
-        {/* <h2 className="mt-4 text-xl font-bold">Alisson Rochinski</h2>
-        <p className="text-muted-foreground text-sm">arochinski</p>
-        <p className="text-muted-foreground mt-1 text-xs">skillbased.net</p>
-
-        <p className="mt-4 text-sm">UI Designer | Let's redesign the world</p>
-        <p className="text-sm">alisson.rochinski@gmail.com</p>
-
-        <p className="text-muted-foreground mt-4 text-sm">
-          2957 seguidores · rochinski.com
-        </p> */}
-        {/* <Wallet />
-        <Socials address={address} /> */}
-        {/* <Wallet>
-          <ConnectWallet>
-            <Avatar className="h-6 w-6" />
-            <Name />
-          </ConnectWallet>
-          <WalletDropdown>
-            <Identity className="px-4 pb-2 pt-3" hasCopyAddressOnClick>
-              <Avatar />
-              <Name />
-              <Address />
-            </Identity>
-            <WalletDropdownDisconnect />
-          </WalletDropdown>
-        </Wallet> */}
-
         <Unauthenticated>
           <ConnectCard />
         </Unauthenticated>
