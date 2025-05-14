7193e25609853d4105d687c0a2c17a3ed4691178
Marcelo <marcelo.vizcarra@inventiva-solutions.com>
2025-05-14 15:21:17 -0500

    feat: refactor Layout component to simplify props and integrate Connections section

--- apps/web/app/(app)/[locale]/(onchain)/(social)/profile/[userId]/layout.tsx
+++ apps/web/app/(app)/[locale]/(onchain)/(social)/profile/[userId]/layout.tsx
@@ -3,17 +3,13 @@ import * as React from "react";
 import AdditionalDetails from "@/app/_pages/profile/ui/AdditionalDetails";
 import Connections from "@/app/_pages/profile/ui/Connections";
 
-export default async function Layout({
-  children,
-  params,
-}: {
-  children: React.ReactNode;
-  params: Promise<{ userId: string }>;
-}) {
-  const { userId } = await params;
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <React.Fragment>
-      <div className="col-start-1 row-start-2 mx-auto hidden w-full lg:block"></div>
+      <div className="col-start-1 row-start-2 mx-auto hidden w-full lg:block">
+        {/* Manager Section */}
+        <Connections />
+      </div>
 
       <div className="col-span-1 col-start-1 h-full overflow-y-hidden border-x lg:col-start-2">
         {children}
@@ -22,9 +18,6 @@ export default async function Layout({
       {/* Sidebar */}
       <div className="col-start-3 row-start-2 hidden h-full overflow-y-auto pt-4 lg:block">
         <div className="mx-auto max-w-xs">
-          {/* Manager Section */}
-          <Connections />
-
           {/* Additional Details Section */}
           <AdditionalDetails />
         </div>
